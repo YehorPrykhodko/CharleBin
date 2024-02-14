182fd5e65bc2b1f2068e75f223af251bd40066af
Yehor Prykhodko <yehor.prykhodko8@etu.univ-lorraine.fr>
2024-02-14 18:09:48 +0200

    not verif commit

--- index.php
+++ index.php
@@ -16,3 +16,7 @@ define('PATH', '');
 define('PUBLIC_PATH', __DIR__);
 require PATH . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 new PrivateBin\Controller();
+
+if ($condition {
+    echo "Condition is true";
+}
